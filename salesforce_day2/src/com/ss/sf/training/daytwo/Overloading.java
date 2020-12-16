fe5c1040f9238c8d40aa70fafe89df435ee53bbb
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2020-12-16 14:30:40 -0700

    Fixed so sums only print once

--- salesforce_day2/src/com/ss/sf/training/daytwo/Overloading.java
+++ salesforce_day2/src/com/ss/sf/training/daytwo/Overloading.java
@@ -7,49 +7,49 @@ public class Overloading {
 	public static void main(String[] args) {
 		Overloading ovl = new Overloading();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter values separated by spaces: ");
 		
+		System.out.println("Enter values separated by spaces: ");
+		Scanner scannedLine = new Scanner(scanner.nextLine());
 		// Create arrays for three types integers, doubles and strings		
 		ArrayList<Integer> intArray = new ArrayList<Integer>();
 		ArrayList<Double> doubleArray = new ArrayList<Double>();
 		ArrayList<String> stringArray = new ArrayList<String>();
-
-		
-		while (scanner.hasNext() == true)  {
-			if (scanner.hasNextInt()) {
-				intArray.add(scanner.nextInt());
+		while (scannedLine.hasNext())  {
+			if (scannedLine.hasNextInt()) {
+				intArray.add(scannedLine.nextInt());
 				// create integer array to allow overloading of method			
-				int size = intArray.size();
-				int[] intArray2 = new int[size];
-				for (int i =0; i<intArray2.length; i++) {
-					intArray2[i] = intArray.get(i);
-				}
-								
-				ovl.addValues(intArray2);
+				
+			} else if (scannedLine.hasNextDouble()) {
+				doubleArray.add(scannedLine.nextDouble());
 				
 				
-			} else if (scanner.hasNextDouble()) {
-				doubleArray.add(scanner.nextDouble());
-				int size = doubleArray.size();
-				// create double array to allow overloading of method
-				double[] double2 = new double[size];
-				for (int i =0; i<double2.length; i++) {
-					double2[i] = doubleArray.get(i);
-				}
-				ovl.addValues(double2);
-				
-			} else if (scanner.hasNext()){
+			} else if (scannedLine.hasNext()){
 				// only one arraylist type, kept string arraylist				
-				stringArray.add(scanner.next());
-				ovl.addValues(stringArray);
+				stringArray.add(scannedLine.next());
+				
 			} else {
-				break;
+				
 				
 			}
 			
-
 		}
-		scanner.close();	
+		int intSize = intArray.size();
+		int[] intArray2 = new int[intSize];
+		for (int i =0; i<intArray2.length; i++) {
+			intArray2[i] = intArray.get(i);
+		}						
+		ovl.addValues(intArray2);
+		int doubleSize = doubleArray.size();
+		// create double array to allow overloading of method
+		double[] double2 = new double[doubleSize];
+		for (int i =0; i<double2.length; i++) {
+			double2[i] = doubleArray.get(i);
+		}
+		ovl.addValues(double2);
+		ovl.addValues(stringArray);
+		scanner.close();
+		
+		
 	}
 	public void addValues(int[] arr) {
 		int sum = 0;
