2c0c41279cfda43f4199b36424996a9d24270a84
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2020-12-17 12:22:25 -0700

    Fixed so user can choose subdirectories to print out

--- salesforce_day3/src/com/ss/sf/training/daythree/PrintDirectory.java
+++ salesforce_day3/src/com/ss/sf/training/daythree/PrintDirectory.java
@@ -19,42 +19,42 @@ public class PrintDirectory {
 	public static void main(String[] args) {
 		//Get present working directory	path	
 		try {String rootDirectory = new File(".").getCanonicalPath();
-			System.out.println("Current Directory: " + rootDirectory + "\nFiles:\n");
-			//	Get files and directories from root	path	
-			File  directory = new File(rootDirectory);
-			String[] directoryFiles = directory.list();
-			//	Print files from directory		
-			for (String file:directoryFiles) {
-				System.out.println(file);
-			}
-			System.out.println("\nType in directory to search:");
+			PrintDirectory pd = new PrintDirectory();
 			Scanner scanner = new Scanner(System.in);
-			//	Ask user for directory to search		
-			String input = scanner.nextLine();
-			//	Check if user input is a file or subdirectory in directory		
-			String subdirectory = rootDirectory + "/" + input;
-			File subdirectoryFiles = new File(subdirectory);
-			String[] filesInSubdirectory = subdirectoryFiles.list();
-				//	Check if user input	is directory or file		
-			if(!(filesInSubdirectory == null)){
-					//	print files in subdirectory				
-				for (String file:filesInSubdirectory) {
-					System.out.println(file);
-					}
-			} else {
-					
-					System.out.println("No such directory exists or is not a directory");
-			}
-				
-			scanner.close();
+			pd.printOutDirectory(rootDirectory,scanner);
+			
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-		
-		
-
 	}
+		public void printOutDirectory(String rootDirectory,Scanner scanner) {
+			System.out.println("Current Directory: " + rootDirectory + "\nFiles:\n");
+			//	Get files and directories from root	path	
+			File  directory = new File(rootDirectory);
+			String[] directoryFiles = directory.list();
+			//	Print files from directory
+			if(!(directoryFiles == null)){
+				//	print files in subdirectory				
+				for (String file:directoryFiles) {
+					System.out.println(file);
+				}
+			} else {
+				System.out.println("No such directory exists or is not a directory");
+				System.exit(0);
+			}
+			System.out.println("\nType in directory to search:");
+			
+			//	Ask user for directory to search		
+			String input = scanner.nextLine();
+			//	Check if user input is a file or subdirectory in directory		
+			String subdirectory = rootDirectory + "/" + input;		
+				
+			scanner.reset();
+			this.printOutDirectory(subdirectory,scanner);
+
+		}
+		
 
 }
